bbc4216486fceff3887ef865fa66c8c5f6fdba2a
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-18 21:27:45 +0300

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -4,10 +4,10 @@
 
 #include <iostream>
 #include <ctime>
-#define SIZE 10
+#define NSIZE 10
+#define MSIZE 15
 using namespace std;
-void randArr (int a[], int sizeArr) {
-	srand(time(0));
+void randArr(int a[], int sizeArr) {
 	int *ptr;
 	ptr = a;
 	for (int i = 0; i<sizeArr; i++, ptr++)
@@ -18,27 +18,57 @@ void printArr(int a[], int sizeArr) {
 	ptr = a;
 	for (int i = 0; i < sizeArr; i++, ptr++)
 		cout << *ptr << " ";
+	cout << endl;
+}
+void sortArr(int a[], int sizeArr) {
+	bool flag = true;
+	int k = sizeArr - 1;
+	while (k > 0)// к - левая граница подмассива
+	{
+		int endI = -1;
+		for (int i = 0; i < k; i++)
+			if (a[i] > a[i + 1]) {
+				//Находим максимальный элемент в части массива от k до SIZE 
+				int tmp = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = tmp;
+				endI = i;
+			}
+		k = endI;
+	}
 }
 void main()
 {
+	srand(time(0));
 	setlocale(LC_ALL, "rus");
-	int myArr[SIZE];
-	int *ptrBegin, *ptrEnd;
-	randArr(myArr, SIZE); // Инициализация массива случайными числами
-	cout << "Исходный массив:" << endl; 
-	printArr(myArr, SIZE);// Вывод массива
-	cout << endl;
-	ptrBegin = myArr;
-	ptrEnd = myArr + SIZE - 1;
-	for (int i = 0; i < SIZE/2; i++, ptrBegin++, ptrEnd--) {
-		int tmp = *ptrBegin;
-		*ptrBegin = *ptrEnd;
-		*ptrEnd = tmp;
-		//printArr(myArr, SIZE);
-		//cout << endl;
+	int aArr[NSIZE];
+	int bArr[MSIZE];
+	int cArr[NSIZE + MSIZE] = {};
+	randArr(aArr, NSIZE);
+	printArr(aArr, NSIZE);
+	sortArr(aArr, NSIZE);
+	cout << "Исходный массив А[n]:" << endl;
+	printArr(aArr, NSIZE);
+
+
+
+	randArr(bArr, MSIZE);
+	printArr(bArr, MSIZE);
+	sortArr(bArr, MSIZE);
+	cout << "Исходный массив B[m]:" << endl;
+	printArr(bArr, MSIZE);
+
+
+	int *ptrA, *ptrB, *ptrC;
+	ptrA = aArr;
+	ptrB = bArr;
+	ptrC = cArr;
+	for (int i = 0; i < NSIZE + MSIZE; i++, ptrC++) {
+		if (*ptrA < *ptrB) *ptrC = *ptrA++;
+			else *ptrC = *ptrB++;
 	}
-	cout << "Итоговый массив:" << endl;
-	printArr(myArr, SIZE);// Вывод массива
-	cout << endl;
+	//sortArr(cArr, NSIZE + MSIZE);
+	cout << endl << "Итоговый массив C[n+m]" << endl;
+	printArr(cArr, NSIZE + MSIZE);
 	system("pause");
 }
